c070c78b223ac4b6950c9aaf674bbe3fa16a1f19
ismawis8 <ismaelpelegrincastillo@gmail.com>
2026-09-24 11:51:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import { createClient } from '@supabase/supabase-js';
 const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://panel.pedidone.es');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
