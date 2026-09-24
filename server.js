20e8fd15c0c15f90322287de7c0a31a92f4d5c1d
ismawis8 <ismaelpelegrincastillo@gmail.com>
2026-09-24 11:43:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ import { createClient } from '@supabase/supabase-js';
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://panel.pedidone.es');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 
 const {
   WEBHOOK_VERIFY_TOKEN,
